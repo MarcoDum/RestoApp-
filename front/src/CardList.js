e92f7497918ddb85aafd3539029e9805710ee603
Stud <stud@netc.eu>
2018-09-14 15:20:26 +0200

    add getCleanRestaurant()

--- front/src/CardList.js
+++ front/src/CardList.js
@@ -16,61 +16,34 @@ class CardList extends Component {
     this.setState({ datas: nextProps.datas });
   }
 
-/*
-  descending = datasState => {
-
-    if (datasState.lenght > 0) {
-
-      datasState.map(data => {
-      let tab5 = [];
-      let tab4 = [];
-      let tab3 = [];
-      let tab2 = [];
-      let tab1 = [];
-      let tab0 = [];
-      let decreaseDatas = [];
-
-        switch (datasState.editorial_rating) {
-          case "5":
-            let tab5 = [...data];
-            break;
-          case "4":
-            tab4 = [...data];
-            break;
-          case "3":
-            tab3 = [...data];
-            break;
-          case "2":
-            tab2 = [...data];
-            break;
-          case "1":
-            tab1 = [...data];
-            break;
-          case "0":
-            tab0 = [...data];
-            break;
-          default:
-            tab0 = [...data];
-        }
-      });
-    }
-    decreaseDatas = [...tab5, ...tab4, ...tab3, ...tab2, ...tab1, ...tab0];
-    return decreaseDatas;
-  };
-*/
+  decrease(datas){
+    JSON.parse(datas)
+    .map(data => ({
+      name: data.name,
+      address1: data.address1,
+      address2: data.address2,
+      image: data.image,
+      imageURL: data.image.url,
+      cat1Name: data.categorisation.primary.name,
+      cat2Name: data.categorisation.secondary.name,
+      rate: data.editorial_rating,
+    }))
+    .filter(data => data.name && data.address1 && data.address2 && data.image && data.imageURL && data.cat1Name && data.cat2Name && data.rate)
+    .sort((a,b) => b.rate - a.rate)
+  }
 
   render() {
     //console.log()
     return (
       <Media>
-        {this.state.datas.map((data, index) => (
+        {this.decrease(this.state.datas).map((data, index) => (
           <div key={index} style={divStyle}>
             <Media.Left>
               {data.image ? (
                 <img
                   width={100}
                   height={80}
-                  src={data.image.url}
+                  src={data.imageURL}
                   alt="thumbnail"
                 />
               ) : (
@@ -86,14 +59,13 @@ class CardList extends Component {
             <Media.Body>
               <Media.Heading>{data.name}</Media.Heading>
               <p>
-                {data.categorisation.primary.name},{" "}
-                {data.categorisation.secondary.name}
+                {data.cat1Name}, {data.cat2Name}
               </p>
               <p>
                 {data.address1} {data.address2}
               </p>
               <ReactStars
-                value={data.editorial_rating}
+                value={data.rate}
                 count={5}
                 size={24}
                 color2={"#ffd700"}
