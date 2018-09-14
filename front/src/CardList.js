8089f2590edae990db29e225a26dfd63e30bb4a1
Stud <stud@netc.eu>
2018-09-14 16:32:05 +0200

    clean code

--- front/src/CardList.js
+++ front/src/CardList.js
@@ -7,7 +7,6 @@ class CardList extends Component {
     super(props);
     this.state = {
       datas: this.props.datas,
-      //datasDescending: [],
       hasError: false
     };
   }
@@ -16,44 +15,44 @@ class CardList extends Component {
     this.setState({ datas: nextProps.datas });
   }
 
-  decrease(datas){
-    JSON.parse(datas)
-    .map(data => ({
-      name: data.name,
-      address1: data.address1,
-      address2: data.address2,
-      image: data.image,
-      imageURL: data.image.url,
-      cat1Name: data.categorisation.primary.name,
-      cat2Name: data.categorisation.secondary.name,
-      rate: data.editorial_rating,
-    }))
-    .filter(data => data.name && data.address1 && data.address2 && data.image && data.imageURL && data.cat1Name && data.cat2Name && data.rate)
-    .sort((a,b) => b.rate - a.rate)
-  }
+  getCleanRestaurant = datas => {
+    return datas
+      .map(data => ({
+        name: data.name,
+        address1: data.address1,
+        address2: data.address2,
+        image: data.image,
+        imageURL: data.image.url,
+        cat1Name: data.categorisation.primary.name,
+        cat2Name: data.categorisation.secondary.name,
+        rate: data.editorial_rating
+      }))
+      .filter(
+        data =>
+          data.name &&
+          data.address1 &&
+          data.address2 &&
+          data.image &&
+          data.imageURL &&
+          data.cat1Name &&
+          data.cat2Name &&
+          data.rate
+      )
+      .sort((a, b) => b.rate - a.rate);
+  };
 
   render() {
-    //console.log()
     return (
       <Media>
-        {this.decrease(this.state.datas).map((data, index) => (
-          <div key={index} style={divStyle}>
+        {this.getCleanRestaurant(this.state.datas).map((data, index) => (
+          <div key={index}>
             <Media.Left>
-              {data.image ? (
-                <img
-                  width={100}
-                  height={80}
-                  src={data.imageURL}
-                  alt="thumbnail"
-                />
-              ) : (
-                <img
-                  width={100}
-                  height={80}
-                  src="http://images.math.cnrs.fr/IMG/png/section1-original.png"
-                  alt="thumbnail"
-                />
-              )}
+              <img
+                width={100}
+                height={80}
+                src={data.imageURL}
+                alt="thumbnail"
+              />
             </Media.Left>
 
             <Media.Body>
@@ -78,8 +77,7 @@ class CardList extends Component {
     );
   }
 }
-const divStyle = {
-  // marginTop: "5px"
-};
 
 export default CardList;
+
+
