35e0771775746e7c86b11ea5a81a29056625cc1b
Stud <stud@netc.eu>
2018-09-19 17:05:08 +0200

    add back with  routes  auth

---
+++ back/sql/init.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE users 
+( 
+    id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+    email VARCHAR(90) UNIQUE, 
+    password VARCHAR(90), 
+    name VARCHAR(90), 
+    lastname VARCHAR(90) 
+);
+
+
+CREATE TABLE restaurants 
+( 
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(90),
+	address1 VARCHAR(90) UNIQUE,
+	address2 VARCHAR(90),
+	area VARCHAR(90),
+	city VARCHAR(90),
+	mainCategory VARCHAR(90),
+	secondaryCategory VARCHAR(90),
+	editorial_rating TINYINT,
+	description VARCHAR(90),
+	annotation TEXT,
+	owner_annotation TEXT,
+	to_website VARCHAR(90),
+	image_url VARCHAR(90),
+	latitude FLOAT,
+	longitude FLOAT
+);
+
