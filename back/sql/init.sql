4a5d06f94a81f326dc2ac9a4460d6e3ac0a2c046
Stud <stud@netc.eu>
2018-09-19 17:28:41 +0200

    add init .sql

--- back/sql/init.sql
+++ back/sql/init.sql
@@ -1,17 +1,17 @@
 
-CREATE TABLE users 
-( 
-    id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+CREATE TABLE users ( 
+    id INTEGER AUTO_INCREMENT, 
     email VARCHAR(90) UNIQUE, 
     password VARCHAR(90), 
     name VARCHAR(90), 
-    lastname VARCHAR(90) 
+    lastname VARCHAR(90),
+	PRIMARY KEY (id)
 );
 
 
 CREATE TABLE restaurants 
 ( 
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT,
     name VARCHAR(90),
 	address1 VARCHAR(90) UNIQUE,
 	address2 VARCHAR(90),
@@ -26,6 +26,7 @@ CREATE TABLE restaurants
 	to_website VARCHAR(90),
 	image_url VARCHAR(90),
 	latitude FLOAT,
-	longitude FLOAT
+	longitude FLOAT,
+	PRIMARY KEY (id)
 );
 
