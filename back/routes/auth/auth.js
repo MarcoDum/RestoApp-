2edd7ba94849ba75ac339490030488aad9c2ee29
Marc-Antoine <mdumais100@gmail.com>
2018-09-20 16:27:12 +0200

    route signin back

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -20,6 +20,24 @@ router.post('/signup',(req, res, next) => {
   })
 })
 
+router.post('/signin', function (req, res, next) {
+  // Ici on gère les informations de l'utilisateur
+
+  // Tester si l'utilisateur existe en BDD  -> Comparer le nom (login) / le password
+  let email = req.body.email;
+  let password = req.body.password;
+
+  connection.query(`select * from users where email= ? and password= ?`, [email, password], function (error, results, fields) {
+    if (error)
+      res.status(500).send(error);
+    else
+      res.status(201).send("youpi");
+  })
+})
+
+
+
+
 module.exports = router;
 
 
